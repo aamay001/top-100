bf79d7fe015855ad690a81e15d490964b038d018
Andy Amaya <contact@andyamaya.com>
2024-12-18 16:51:40 -0800

    Adds vite legacy plugin to provide automatic polyfills as necessary.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy';
+
+const legacyPluginOptions = {
+  modernTargets: "edge>=79, firefox>=67, chrome>=64, safari>=12, chromeAndroid>=64, iOS>=12",
+  modernPolyfills: true,
+  renderLegacyChunks: false,
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [legacy(legacyPluginOptions), react()],
+});
