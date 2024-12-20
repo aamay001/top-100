ba60c0eb948f1be40ff5c59cdc4eec81f7903424
Andy Amaya <contact@andyamaya.com>
2024-12-20 05:17:38 -0800

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router';
 import { Theme } from 'tamagui';
 
 import AlbumDataProvider from './contexts/album-data/album-data-provider';
-import Home from './pages/home.tsx';
+import Home from './pages/home';
 
 import './app.css';
 
