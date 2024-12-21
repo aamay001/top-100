56eb891cb028f3b5d27e1d414931402f6a7e8e00
Andy Amaya <contact@andyamaya.com>
2024-12-21 15:25:49 -0800

    Constant strings added.

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,5 +1,11 @@
 const ALBUM_DATA_SOURCE = 'https://itunes.apple.com/us/rss/topalbums/limit=100/json';
+const SEARCH_INPUT_PLACEHOLDER = 'search albums by name';
+const NO_SEARCH_REASULTS_FOUND = 'No albums found!\n¯\\_(ツ)_/¯\nTry a different search.';
+const SEARCH_INFO_TEXT = 'Filters are applited to the current search results in order:\nArtist, then Category, then Year\n\nChanging the search term will clear the filters.';
 
 export {
   ALBUM_DATA_SOURCE,
+  SEARCH_INPUT_PLACEHOLDER,
+  NO_SEARCH_REASULTS_FOUND,
+  SEARCH_INFO_TEXT,
 }
