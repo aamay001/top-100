6654718b16f0ad6590035452f48e66724fd295d9
Andy Amaya <contact@andyamaya.com>
2024-12-22 14:54:09 -0800

    Save color mode for recall.

--- src/contexts/color-mode/color-mode-provider.tsx
+++ src/contexts/color-mode/color-mode-provider.tsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
 import ColorModeContext from "./color-mode-context";
 
+type ColorMode = 'light' | 'dark';
+
 interface ColorModeProviderProps {
   children: React.ReactNode,
-  initialValue: 'dark' | 'light'
+  initialValue: ColorMode
 }
 
+const COLOR_MODE_SAVE_KEY = 'color-mode';
+
 const ColorModeProvider: React.FC<ColorModeProviderProps> = ({ children, initialValue }) => {
-  const [colorMode, setColorMode] = useState<'dark' | 'light'>(initialValue);
+  const saved: string | null = localStorage.getItem(COLOR_MODE_SAVE_KEY);
+  const [colorMode, setColorMode] = useState<ColorMode>(saved as ColorMode || initialValue);
   const [ colorModeBackgroundColor, setColorModeBackgroundColor] = useState<'gainsboro' | '#28282B'>('#28282B');
 
   const toggleColorMode = () => {
     if (colorMode === 'dark') {
       setColorMode('light');
       setColorModeBackgroundColor('gainsboro');
+      localStorage.setItem(COLOR_MODE_SAVE_KEY, 'light');
     } else {
       setColorMode('dark');
       setColorModeBackgroundColor('#28282B');
+      localStorage.setItem(COLOR_MODE_SAVE_KEY, 'dark');
     }
   }
 
