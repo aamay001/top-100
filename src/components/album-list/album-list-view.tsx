2fb46ba3b24f2db2f04ae96380aef11393cb13ea
Andy Amaya <contact@andyamaya.com>
2024-12-21 16:26:43 -0800

    Adds the search to album list view.

--- src/components/album-list/album-list-view.tsx
+++ src/components/album-list/album-list-view.tsx
@@ -1,11 +1,13 @@
-import { H1, Spinner, XStack, YStack } from "tamagui";
+import { useEffect, useState } from "react";
+import { H1, Spinner, XStack, YStack, Paragraph } from "tamagui";
 
 import AlbumList from "./album-list";
-import { useEffect, useState } from "react";
 import AlbumView from "../album/album-view";
+import Search from "../search/search";
+import { useOrientation } from "../../hooks/use-orientation";
+import { NO_SEARCH_REASULTS_FOUND } from "../../constants/strings";
 
 import "../../styles/album-list.view.scss";
-import { useOrientation } from "../../hooks/use-orientation";
 
 interface AlbumListViewProps {
   title: string;
@@ -26,14 +28,21 @@ const AlbumListView: React.FC<AlbumListViewProps> = ({
 }) => {
   const [selectedAlbum, setSelectedAlbum] = useState<SelectedAlbumState | undefined>(undefined);
   const [albumInModal, setAlbumInModal] = useState<boolean>(false);
+  const [searchResults, setSearchResults] = useState<Album[] | null>(null);
 
   useEffect(() => {
     setSelectedAlbum({
-      album: albums[0],
+      album: searchResults
+        ? searchResults[0]
+        : albums[0],
       position: 1,
       openModal: false,
     });
-  }, [albums]);
+  }, [albums, searchResults]);
+
+  const onSearchResults = (results: Album[] | null) => {
+    setSearchResults(results);
+  }
 
   useOrientation((type) => {
     if (type === 'landscape-secondary' || type === 'landscape-primary') {
@@ -66,27 +75,42 @@ const AlbumListView: React.FC<AlbumListViewProps> = ({
   return (
     <>
       <H1 paddingLeft={10} size="$9">{title}</H1>
-      <XStack className="album-list-view-container">
-      {isLoading
-        ? (
-          <Spinner size="large" marginTop={100} />
-      ) : (
-        <>
-          <YStack className="album-list-view-left-pane">
-            <AlbumList albums={albums} onAlbumClicked={onAlbumClicked} />
-          </YStack>
-          <YStack className="album-list-view-right-pane">
-            <AlbumView
-              album={selectedAlbum?.album}
-              listPosition={selectedAlbum?.position}
-              asModal={albumInModal}
-              openModal={selectedAlbum?.openModal}
-              onModalClosed={onModalClosed}
-            />
-          </YStack>
-        </>
-      )}
-      </XStack>
+      <YStack justifyContent="center" alignItems="center">
+        <Search 
+          data={albums} 
+          dataKey="name" 
+          onSearch={onSearchResults} 
+          className="album-list-view-search"
+        />
+        <XStack className="album-list-view-pane-container">
+        {isLoading
+          ? (
+            <Spinner size="large" marginTop={100} />
+        ) : (
+          <>
+            <YStack className="album-list-view-left-pane">
+              <AlbumList 
+                albums={searchResults || albums} 
+                onAlbumClicked={onAlbumClicked}
+              />
+              {searchResults && searchResults.length === 0 &&
+                <Paragraph size="$8" textAlign="center" paddingTop="$5" whiteSpace="pre-line">
+                  {NO_SEARCH_REASULTS_FOUND}
+                </Paragraph>}
+            </YStack>
+            <YStack className="album-list-view-right-pane">
+              <AlbumView
+                album={selectedAlbum?.album}
+                listPosition={selectedAlbum?.position}
+                asModal={albumInModal}
+                openModal={selectedAlbum?.openModal}
+                onModalClosed={onModalClosed}
+              />
+            </YStack>
+          </>
+        )}
+        </XStack>
+      </YStack>
     </>
   );
 };
